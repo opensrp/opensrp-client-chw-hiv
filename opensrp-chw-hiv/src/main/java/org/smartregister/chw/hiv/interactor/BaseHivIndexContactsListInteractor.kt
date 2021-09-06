a323349b2079b0862280df4e4acf19864d2a150d
Ilakoze Jumanne <ilakozejumanne@gmail.com>
2021-09-06 07:47:27 +0300

    Added more code documentation

--- opensrp-chw-hiv/src/main/java/org/smartregister/chw/hiv/interactor/BaseHivIndexContactsListInteractor.kt
+++ opensrp-chw-hiv/src/main/java/org/smartregister/chw/hiv/interactor/BaseHivIndexContactsListInteractor.kt
@@ -7,28 +7,36 @@ import org.smartregister.chw.hiv.contract.BaseIndexClientsContactListContract
 import org.smartregister.chw.hiv.dao.HivIndexDao
 import org.smartregister.chw.hiv.domain.HivIndexContactObject
 
+/**
+ * Created by cozej4 on 2021-07-13.
+ *
+ * @cozej4 https://github.com/cozej4
+ *
+ * This is the interactor for  [org.smartregister.chw.hiv.activity.BaseIndexContactsListActivity]
+ * implements [BaseIndexClientsContactListContract.Interactor]
+ */
 open class BaseHivIndexContactsListInteractor @VisibleForTesting internal constructor(
-    var appExecutors: AppExecutors
+        var appExecutors: AppExecutors
 ) : BaseIndexClientsContactListContract.Interactor {
 
-    constructor() : this(AppExecutors()) {}
+    constructor() : this(AppExecutors())
 
     override fun getClientIndexes(
-        hivClientBaseEntityId: String?,
-        context: Context?,
-        callBack: BaseIndexClientsContactListContract.InteractorCallBack?
+            hivClientBaseEntityId: String?,
+            context: Context?,
+            callBack: BaseIndexClientsContactListContract.InteractorCallBack?
     ) {
         val runnable = Runnable {
             val indexClients = getIndexClient(hivClientBaseEntityId)
 
             appExecutors.mainThread()
-                .execute { callBack!!.onDataFetched(indexClients) }
+                    .execute { callBack!!.onDataFetched(indexClients) }
         }
         appExecutors.diskIO().execute(runnable)
     }
 
     protected fun getIndexClient(
-        hivClientBaseEntityId: String?
+            hivClientBaseEntityId: String?
     ): List<HivIndexContactObject?> {
         try {
             Thread.sleep(2000)
--- opensrp-chw-hiv/src/main/java/org/smartregister/chw/hiv/interactor/BaseIndexContactProfileInteractor.kt
+++ opensrp-chw-hiv/src/main/java/org/smartregister/chw/hiv/interactor/BaseIndexContactProfileInteractor.kt
@@ -5,30 +5,29 @@ import org.smartregister.chw.anc.util.AppExecutors
 import org.smartregister.chw.hiv.contract.BaseIndexContactProfileContract
 import org.smartregister.chw.hiv.contract.BaseIndexContactProfileContract.InteractorCallback
 import org.smartregister.chw.hiv.domain.HivIndexContactObject
-import org.smartregister.domain.AlertStatus
-import java.util.*
 
 /**
  * Created by cozej4 on 2021-07-13.
  *
  * @cozej4 https://github.com/cozej4
  *
- * This is the presenter for  [org.smartregister.chw.hiv.activity.BaseIndexContactProfileActivity]
+ * This is the interactor for  [org.smartregister.chw.hiv.activity.BaseIndexContactProfileActivity]
+ * implements [BaseIndexContactProfileContract.Interactor]
  */
 open class BaseIndexContactProfileInteractor @VisibleForTesting internal constructor(
-    var appExecutors: AppExecutors
+        var appExecutors: AppExecutors
 ) : BaseIndexContactProfileContract.Interactor {
 
     constructor() : this(AppExecutors())
 
     override fun refreshProfileView(
-        hivIndexContactObject: HivIndexContactObject?,
-        isForEdit: Boolean,
-        callback: InteractorCallback?
+            hivIndexContactObject: HivIndexContactObject?,
+            isForEdit: Boolean,
+            callback: InteractorCallback?
     ) {
         val runnable = Runnable {
             appExecutors.mainThread()
-                .execute { callback!!.refreshProfileTopSection(hivIndexContactObject) }
+                    .execute { callback!!.refreshProfileTopSection(hivIndexContactObject) }
         }
         appExecutors.diskIO().execute(runnable)
     }
